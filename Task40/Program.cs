320d541ec24c8a5ccf100659ff78f189016c036d
Dmitry Budyakov <dnikbu@yandex.ru>
2022-09-16 23:00:00 +0300

    Task40 - через bool метод

--- Task40/Program.cs
+++ Task40/Program.cs
@@ -5,17 +5,23 @@
 // Теорема о неравенстве треугольника: каждая сторона треугольника
 // меньше суммы двух других сторон.
 
-void CheckTriangleExist(int num1, int num2, int num3)
+bool CheckTriangleExist(int num1, int num2, int num3)
 {
-    if (num1 < num2 + num3 && num2 < num1 + num3 && num3 < num1 + num2) Console.WriteLine("Такой треугольник может существовать.");
-    else Console.WriteLine("Такой треугольник не может существовать.");
+    bool check = false;
+    if (num1 < num2 + num3 && num2 < num1 + num3 && num3 < num1 + num2) return true;
+    else return check;
 }
 
-Console.Write("Введите первое число: ");
+// вводим стороны треугольника
+Console.Write("Введите длину 1-ой стороны треугольника: ");
 int number1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите второе число: ");
+Console.Write("Введите длину 2-ой стороны треугольника: ");
 int number2 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите третье число: ");
+Console.Write("Введите длину 3-ой стороны треугольника: ");
 int number3 = Convert.ToInt32(Console.ReadLine());
 
-CheckTriangleExist(number1, number2, number3);
+// результат проверки
+bool verdict = CheckTriangleExist(number1, number2, number3);
+// вывод в консоль
+if (verdict) Console.WriteLine("Такой треугольник может существовать.");
+else Console.WriteLine("Такой треугольник не может существовать.");
