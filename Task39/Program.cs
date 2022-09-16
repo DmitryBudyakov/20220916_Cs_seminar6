a9f48a3d63e9ffa3e6f66f2b45909651850087e0
Dmitry Budyakov <dnikbu@yandex.ru>
2022-09-16 20:33:38 +0300

    Task39 - с циклом for

--- Task39/Program.cs
+++ Task39/Program.cs
@@ -43,8 +43,25 @@ void ReverseArray(int[] array)
         index2--;
     }
 }
+void ReverseArrayFor(int[] array)
+{
+    int size = array.Length;
+    // int index1 = 0;
+    // int index2 = size - 1;
+
+    for (int i = 0; i < size / 2; i++)
+    {
+        int temp = array[i];
+        array[i] = array[size - 1 - i];
+        array[size - 1 - i] = temp;
+
+    }
+}
+
 
 int[] arr = CreateArrayRndInt(5, 1, 9);
 PrintArray(arr);
 ReverseArray(arr);
+PrintArray(arr);
+ReverseArrayFor(arr);
 PrintArray(arr);
