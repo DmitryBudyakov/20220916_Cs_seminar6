76e95766ea6b93a0675b11720d830fda6147954d
Dmitry Budyakov <dnikbu@yandex.ru>
2022-09-16 21:37:01 +0300

    Task40  - через void метод

---
+++ Task40/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 40:
+// Напишите программу, которая принимает на вход три
+// числа и проверяет, может ли существовать треугольник с сторонами
+// такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника
+// меньше суммы двух других сторон.
+
+void CheckTriangleExist(int num1, int num2, int num3)
+{
+    if (num1 < num2 + num3 && num2 < num1 + num3 && num3 < num1 + num2) Console.WriteLine("Такой треугольник может существовать.");
+    else Console.WriteLine("Такой треугольник не может существовать.");
+}
+
+Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+CheckTriangleExist(number1, number2, number3);
