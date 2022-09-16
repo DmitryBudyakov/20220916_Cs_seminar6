daa8c9ea4734f4037ba3b3dcf10c5da808960115
Dmitry Budyakov <dnikbu@yandex.ru>
2022-09-16 22:32:25 +0300

    Task42 - через переменные и массив

---
+++ Task42/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 42:
+// Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+
+int ConvertDecToBin(int num)
+{
+    int result = 0;
+    int count = 1;
+    while (num != 0)
+    {
+        result += (num % 2) * count;
+        num /= 2;
+        count *= 10;
+    }
+    return result;
+}
+
+
+Console.Clear();
+int number = default;
+Console.Write("Введите целое число: ");
+    while (!int.TryParse(Console.ReadLine(), out number))
+    {
+        Console.Write("Ошибка ввода.\nВведите целое число: ");
+    }
+
+int resultNum = ConvertDecToBin(number);
+Console.WriteLine(resultNum);
